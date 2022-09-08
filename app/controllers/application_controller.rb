aa6cee43af7f18f8427e039dbd46e553893ff0cb
Elizabeth Wandia Mugo <wwandia173@gmail.com>
2022-09-08 12:56:34 +0300

    migrate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
+require './config/environment'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  configure do
+    set :public_folder, 'public'
+    enable :sessions
+    set :session_secret, SecureRandom.hex(24)
   end
 
 end
---
+++ db/migrate/20220908095556_create_todo_categories.rb
@@ -0,0 +1,4 @@
+class CreateTodoCategories < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
